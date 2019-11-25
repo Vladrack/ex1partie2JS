0fe065fbba24dc3cead2fdcba5b8a4323c217863
Vladrack <frg.cedric@gmail.com>
2019-11-25 14:26:51 +0100

    partie2ex1JS

---
+++ assets/JS/partie2Exo1JS.js
@@ -0,0 +1,6 @@
+function changeImage(){
+    document.getElementById('naturalImage').src = 'assets/img/appartement.jpg';
+    }
+    function changeAgain() {
+        document.getElementById('naturalImage').src = 'assets/img/manhattan.jpg';
+    }
